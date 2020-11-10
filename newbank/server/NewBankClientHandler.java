65a59856cc90f2a88bcfe3cd9e3d8a6e04182744
Colin <colin.warburton@hotmail.co.uk>
2020-11-10 20:32:30 +0000

    Moved login code into sub-function function and restructured run() such that users can retry failed login attempts

--- newbank/server/NewBankClientHandler.java
+++ newbank/server/NewBankClientHandler.java
@@ -11,8 +11,14 @@ public class NewBankClientHandler extends Thread{
 	private NewBank bank;
 	private BufferedReader in;
 	private PrintWriter out;
-	
-	
+
+	/**
+	 * Constructs a NewBankClientHandler class to handle the connection from a
+	 * client
+	 *
+	 * @param s the network socket of the connection
+	 * @throws IOException
+	 */
 	public NewBankClientHandler(Socket s) throws IOException {
 		bank = NewBank.getBank();
 		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -20,34 +26,24 @@ public class NewBankClientHandler extends Thread{
 	}
 	
 	public void run() {
-		// keep getting requests from the client and processing them
 		try {
-			// ask for user name
-			out.println("Enter Username");
-			String userName = in.readLine();
-			// ask for password
-			out.println("Enter Password");
-			String password = in.readLine();
-			out.println("Checking Details...");
-			// authenticate user and get customer ID token from bank for use in subsequent requests
-			CustomerID customer = bank.checkLogInDetails(userName, password);
-			// if the user is authenticated then get requests from the user and process them 
-			if(customer != null) {
-				out.println("Log In Successful. What do you want to do?");
-				while(true) {
+			CustomerID customer = null;
+			while (true) {
+				// keep getting requests from the client and processing them
+				if (customer == null) {
+					customer = handleLogin();
+				} else if (customer != null) {
+					// if the user is authenticated then get requests from the user and process them
+					out.println("What do you want to do?");
 					String request = in.readLine();
 					System.out.println("Request from " + customer.getKey());
-					String responce = bank.processRequest(customer, request);
-					out.println(responce);
+					String response = bank.processRequest(customer, request);
+					out.println(response);
 				}
-			}
-			else {
-				out.println("Log In Failed");
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
 				in.close();
 				out.close();
@@ -56,6 +52,35 @@ public class NewBankClientHandler extends Thread{
 				Thread.currentThread().interrupt();
 			}
 		}
+	}
+
+	/**
+	 * Presents the login UI asking for username and password
+	 *
+	 * @return the customerID of the logged in user or null if the login failed
+	 * e.g. incorrect username or password
+	 *
+	 * TODO usernames should be case insensitive
+	 *
+	 * TODO limit login attempts per session to mitigate brute force attacks
+	 */
+	private CustomerID handleLogin() throws IOException {
+		// ask for user name
+		out.println("Enter Username");
+		String userName = in.readLine();
+		// ask for password
+		out.println("Enter Password");
+		String password = in.readLine();
+		out.println("Checking Details...");
+		// authenticate user and get customer ID token from bank for use in
+		// subsequent requests
+		CustomerID customer = bank.checkLogInDetails(userName, password);
+		if (customer == null) {
+			out.println("Log In Failed");
+			return null;
+		}
+		out.println("Log In Successful.");
+		return customer;
 	}
 
 }
