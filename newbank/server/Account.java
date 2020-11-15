2b74fdc1241a720301c8980ce2fd70dea46220d3
Alexandra McCarroll <34590557+AlexMcCarroll@users.noreply.github.com>
2020-11-15 17:32:51 +0000

    18-withdrawals (#8)

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -1,17 +1,37 @@
 package newbank.server;
 
 public class Account {
-	
+
 	private String accountName;
 	private double openingBalance;
+	private double currentBalance;
 
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
 		this.openingBalance = openingBalance;
+		this.currentBalance = openingBalance;
 	}
-	
+
 	public String toString() {
 		return (accountName + ": " + openingBalance);
 	}
+	public String getAccountName(){
+		return(accountName);
+	}
 
+	/**
+	 * Method to retrieve the current balance of an account.
+	 * @return currentBalance
+	 */
+	public double getCurrentBalance() {
+		return currentBalance;
+	}
+
+	/**
+	 *  Method to change the current balance. - Useful for transactions etc
+	 * @param currentBalance
+	 */
+	public void setCurrentBalance(double currentBalance) {
+		this.currentBalance = currentBalance;
+	}
 }
