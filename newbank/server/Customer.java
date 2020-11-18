03b1c3f4ad79c42c68ec718ebee069df0207046a
Akshay Padki <akshay.padki@ocado.com>
2020-11-18 11:53:48 +0000

    latest from 30 into 19

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -1,38 +1,101 @@
 package newbank.server;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 public class Customer {
+	private String name;
+	private String password;
 	private ArrayList<Account> accounts;
-	public Customer() {
+	public Customer(String name, String password) {
+		this.name = name;
+		this.password = password;
 		accounts = new ArrayList<>();
 	}
 	public String accountsToString() {
 		String s = "";
-		for(Account a : accounts) {
-			s += a.toString();
-		}
-		return s;
-	}
-	//Method to print all current balances.
-	public String AllCurrentBalanceToString(){
-		String s = ""; //Blank string is initiated.
-		for(Account a : accounts){//Cycle through all the customers accounts
-			s += a.getAccountName() + ": " + a.getCurrentBalance() + "\n"; //Each current balance of each type account is printed.
-		}
-		return s;
-	}
-	//Method to get current balance of specific account type.
-	public String CurrentBalanceToString(String A){ //Parameter A is the chosen account type.
-		String x = "Error No Account Found"; // Still Needs work.
-		String s = null;
-		for(Account a : accounts) { //Cycle through each of the customers accounts.
-			if (a.getAccountName().equals(A)){ // If the chosen account is matched with an actual account balance is returned.
-				s = a.getAccountName() + ": " + a.getCurrentBalance() + "\n";
-				return s;
-			}
+		for (Account a : accounts) {
+			s += a.toString() + "\n";
 		}
 		return s;
 	}
 	public void addAccount(Account account) {
 		accounts.add(account);
+	}
+	// Creates a method for a deposit which can be accessed in NewBank
+	public String Deposit(String accType, String amount){
+		String s = null;
+		for (Account a : accounts) {
+			if (a.getAccountName().equals(accType)) {
+				s = a.getAccountName() + ": " + (a.getCurrentBalance() + Double.parseDouble(amount));
+				return s;
+			}
+		}
+		return s;
+	}
+	/**
+	 * Updates customer's password
+	 *
+	 * @param password new value
+	 * @throws InvalidParameterException if password is 4 characters or less
+	 */
+	public void updatePassword(String password) throws InvalidParameterException {
+		if (password.length() > 4) {
+			this.password = password;
+		} else {
+			throw new InvalidParameterException();
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * Method to allow access to update account balances
+	 * @param account
+	 */
+	public void updateAccount(Account account){
+		for(int i =0; i <accounts.size(); i ++) {
+			if (account.getAccountName().equals(accounts.get(i).getAccountName())){
+				accounts.set(i,account);
+			}
+		}
+	}
+	/**
+	 * Method to allow account to be transacted upon to be accessed
+	 * @param accountName
+	 * @return
+	 */
+	public Account getTransactionAccount(String accountName) {
+		Account account = null;
+		for (Account a : accounts) {
+			if (accountName.equals(a.getAccountName())) {
+				account = a;
+			}
+		}
+		return account;
+	}
+	/**
+	 * Method to return the current balance
+	 * @param accountType
+	 * @return
+	 */
+	public String currentBalance(String accountType) {
+		for (Account a : accounts) {
+			if (a.getAccountName().equals(accountType)) {
+				String s = a.getAccountName() + ": " + a.getCurrentBalance();
+				return s;
+			}
+		}
+		return null;
+	}
+	public String Withdraw(String accType, String amount){
+		for (Account a : accounts) {
+			if (a.getAccountName().equals(accType)) {
+				String s = a.getAccountName() + ": " + (a.getCurrentBalance() - Double.parseDouble(amount));
+				return s;
+			}
+		}
+		return null;
 	}
 }
--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -1,60 +1,105 @@
 package newbank.server;
+import java.io.*;
 import java.util.HashMap;
 public class NewBank {
 	private static final NewBank bank = new NewBank();
 	private HashMap<String,Customer> customers;
+	private String account;
+	public String AccountType; //New string to help with type of account typed in by user - useful for currentbalance and possibly more.
 	private NewBank() {
 		customers = new HashMap<>();
 		addTestData();
 	}
 	private void addTestData() {
-		Customer bhagy = new Customer();
+		Customer bhagy = new Customer("Bhagy", "bhagy");
 		bhagy.addAccount(new Account("Main", 1000.0));
-		customers.put("Bhagy", bhagy);
-		Customer christina = new Customer();
+		customers.put(bhagy.getName(), bhagy);
+		Customer christina = new Customer("Christina", "christina");
 		christina.addAccount(new Account("Savings", 1500.0));
-		customers.put("Christina", christina);
-		Customer john = new Customer();
+		customers.put(christina.getName(), christina);
+		Customer john = new Customer("John", "john");
 		john.addAccount(new Account("Checking", 250.0));
-		john.addAccount(new Account("Main", 1250.0));
-		john.addAccount(new Account("Savings", 50.0));
-		customers.put("John", john);
+		john.addAccount(new Account("Savings", 111));
+		customers.put(john.getName(), john);
 	}
 	public static NewBank getBank() {
 		return bank;
 	}
 	public synchronized CustomerID checkLogInDetails(String userName, String password) {
 		if(customers.containsKey(userName)) {
-			return new CustomerID(userName);
+			if (customers.get(userName).getPassword().equals(password)) {
+				return new CustomerID(userName);
+			}
 		}
 		return null;
 	}
-	public String AccountType; //New string to help with type of account typed in by user - useful for currentbalance and possibly more.
-	// commands from the NewBank customer are processed in this method.
+	/**
+	 * commands from the NewBank customer are processed in this method
+	 * @param customer
+	 * @param request
+	 * @return
+	 */
 	public synchronized String processRequest(CustomerID customer, String request) {
-		if(customers.containsKey(customer.getKey())) {
-			switch(request) {
-				case "SHOWMYACCOUNTS" : return showMyAccounts(customer);
-				case "Show All Current Balances" : return showallMyBal(customer);
-				case "Show Current Balance for Main" :
-					AccountType = "Main";
-					return ShowMyBal(customer, AccountType);
-				case "Show Current Balance for Checking" :
-					AccountType = "Checking";
-					return ShowMyBal(customer, AccountType);
-				case "Show Current Balance for Savings" :
-					AccountType = "Savings";
-					return ShowMyBal(customer, AccountType);
-				default : return "FAIL";
+		if (!customers.containsKey(customer.getKey())) {
+			return "FAIL";
+		}
+		String[] arguments = request.split(" ");
+		if (arguments.length >= 1) {
+			switch (arguments[0]) {
+				case "SHOWMYACCOUNTS":
+					return showMyAccounts(customer);
+				case "DEPOSIT":
+					return depositTransaction(customer, arguments[1],arguments[2]);
+				case "CHANGEPASSWORD":
+					if (arguments.length >= 2) {
+						return changePassword(customer, arguments[1]);
+					} else {
+						return "FAIL New password not specified";
+					}
+				case "WITHDRAW":
+					return withdrawTransaction(customer, arguments[1], arguments[2]);
+				case "SHOWSTATUS":
+					return showCurrentStatus(customer);
+				case "SHOWCURRENTBALANCE":
+					if (arguments.length == 2){
+						return ShowMyBal(customer, arguments[1]); //Passes the account type to ShowMyBal to get curr bal.
+					}
+					return "Incorrect Usage"; // Handling if SHOWCURRENTBALANCE does not have just account type after
 			}
 		}
 		return "FAIL";
 	}
+	/**
+	 * Updates a customers password
+	 *
+	 * @param customer the customerID to be updated
+	 * @param newPassword must be longer than 4 characters
+	 * @return a status message for display to the user
+	 */
+	private String changePassword(CustomerID customer, String newPassword) {
+		try {
+			customers.get(customer.getKey()).updatePassword(newPassword);
+			return "Password updated";
+		} catch (Exception e) {
+			return "FAIL password not updated";
+		}
+	}
 	private String showMyAccounts(CustomerID customer) {
 		return (customers.get(customer.getKey())).accountsToString();
 	}
-	//Handles retrieving all the current balances.
-	private String showallMyBal(CustomerID customer){return (customers.get(customer.getKey())).AllCurrentBalanceToString();}
 	//Handles retrieving the current balance for specific type of account.
-	private String ShowMyBal(CustomerID customer, String A){return (customers.get(customer.getKey())).CurrentBalanceToString(A);}
+	private String ShowMyBal(CustomerID customer, String AccType){
+		return (customers.get(customer.getKey())).currentBalance(AccType);
+	}
+	// method to deposit money, takes account type and amount to deposit
+	// accesses the Deposit method in Customer which returns correct format for this function
+	private String depositTransaction(CustomerID customer,String accType ,String amount){
+		return (customers.get(customer.getKey()).Deposit(accType, amount));
+	}
+	private String showCurrentStatus(CustomerID customer) {
+		return (customers.get(customer.getKey())).currentBalance(account);
+	}
+	private String withdrawTransaction(CustomerID customer,String accType ,String amount){
+		return (customers.get(customer.getKey()).Withdraw(accType, amount));
+	}
 }
