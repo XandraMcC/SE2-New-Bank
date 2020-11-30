d23120723b4acf8aebf5825bcc9edf092b6e4d39
Colin W <cjw205@bath.ac.uk>
2020-11-30 20:40:21 +0000

    Adding application logic classes for the microloan market (#25)
    * Adding application logic classes for the microloan market
    
    * Add comments to loan code

---
+++ newbank/server/LoanMarket.java
@@ -0,0 +1,43 @@
+package newbank.server;
+
+import java.util.ArrayList;
+
+public class LoanMarket {
+
+  private ArrayList<LoanOffer> loanOffers;
+
+  /**
+   * Constructor
+   */
+  public LoanMarket() {
+    loanOffers = new ArrayList<>();
+  }
+
+  /**
+   * Adds a loan offer to the market
+   * @param loanOffer the new loan offer
+   */
+  public void addLoanOffer(LoanOffer loanOffer) {
+    loanOffers.add(loanOffer);
+  }
+
+  /**
+   * Removes a loan offer from the market
+   * @param loanOffer to remove
+   * @throws Exception if the offer was not on the market
+   */
+  public void removeLoanOffer(LoanOffer loanOffer) throws Exception {
+    if (!loanOffers.contains(loanOffer)) {
+      throw new Exception("LoanOffer not in market");
+    }
+    loanOffers.remove(loanOffer);
+  }
+
+  /**
+   * @return a list of all the loan offers on the market
+   */
+  ArrayList<LoanOffer> getLoanOffers() {
+    return loanOffers;
+  }
+
+}
---
+++ newbank/server/LoanOffer.java
@@ -0,0 +1,46 @@
+package newbank.server;
+
+public class LoanOffer {
+
+  private Currency amount; // The amount of money being offered
+  private float interestRate; // The rate at which this is being offered APR e.g. 5% would be 0.05
+  private Customer offerer; // The person offering the loan
+  private String accountName; // The account the loan is being
+
+  /**
+   * Constructor for a LoanOffer, a micro loan offer is an offer by a user loan money at an interest rate
+   * @param offerer the customer offering to loan the money
+   * @param accountName the account from whcih the loan funds are paid
+   * @param amount the maximum amount that is being offered in loans
+   * @param interestRate the rate at which the loan is offered
+   */
+  public LoanOffer(Customer offerer, String accountName, Currency amount, float interestRate) {
+    this.offerer = offerer;
+    this.accountName = accountName;
+    this.amount = new Currency(amount);
+    this.interestRate = interestRate;
+  }
+
+  /**
+   * Takes out a new loan from the offer
+   * @param receivingAccount the account to receive the funds
+   * @param loanName the name of the loan
+   * @param loanAmount the amount to be loaned
+   * @return the new loan
+   * @throws Exception if the amount exceeds the maximum offered or the loan offerer does not have enough funds
+   */
+  public Loan takeLoan(Account receivingAccount, String loanName, Currency loanAmount) throws Exception {
+    if (amount.getValue() < loanAmount.getValue()) {
+      throw new Exception("Loan requested exceeds loan amount on offer");
+    }
+    if (!offerer.getAccount(accountName).hasFunds(loanAmount)) {
+      throw new Exception("Loan offerer does not have enough funds in their account!");
+    }
+    amount.subtract(loanAmount); // Reduce the amount offered in the loan market
+    Loan newLoan = new Loan(loanName, loanAmount, interestRate);
+    offerer.getAccount(accountName).withdraw(loanAmount);
+    receivingAccount.deposit(loanAmount);
+    return newLoan;
+  }
+
+}
