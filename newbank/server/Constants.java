c826d4a80f9994ec9970fee23fcecddfa3fd78ca
AlexMcCarroll <alexandra@mccarrollhk.com>
2020-12-03 16:10:28 +0000

    add cmd

---
+++ newbank/server/Constants.java
@@ -0,0 +1,16 @@
+package newbank.server;
+
+public class Constants {
+
+    private Constants() {}
+
+    public static final String ACCOUNTEXISTS = "Fail: Already have an account with that name";
+    public static final String DEPOSIT = "deposit";
+    public static final String END = "end";
+    public static final String NOTLOGGEDIN = "Fail: Not logged in";
+    public static final String NOTRECOGNISED = "Fail: Command not recognised";
+    public static final String NOCOMMAND = "Fail: Command not entered";
+    public static final String WITHDRAW = "withdraw";
+    public static final String CASH = "cash";
+
+}
