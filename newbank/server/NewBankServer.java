a0016b4ae92b3030a71f19318b8226eb49c1662c
Colin <colin.warburton@hotmail.co.uk>
2020-12-10 14:51:40 +0000

    Added comments to NewBankServer.java

--- newbank/server/NewBankServer.java
+++ newbank/server/NewBankServer.java
@@ -4,21 +4,33 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class NewBankServer extends Thread{
+public class NewBankServer extends Thread {
 	
 	private final ServerSocket server;
-	
+
+	/**
+	 * Constructor sets up a NewBankServer
+	 * @param port to listen for connections on
+	 * @throws IOException if there is a network error
+	 */
 	public NewBankServer(int port) throws IOException {
 		server = new ServerSocket(port);
 	}
 
+	/**
+	 * Starts a new NewBankServer thread on port 14002
+	 * @param args not used
+	 * @throws IOException if there is an error reading data
+	 */
 	public static void main(String[] args) throws IOException {
-		// starts a new NewBankServer thread on a specified port number
 		new NewBankServer(14002).start();
 	}
-	
+
+	/**
+	 * Main loop which starts up a new client handler thread to receive incoming
+	 * connections and process requests
+	 */
 	public void run() {
-		// starts up a new client handler thread to receive incoming connections and process requests
 		System.out.println("New Bank Server listening on " + server.getLocalPort());
 		try {
 			while(true) {
@@ -38,5 +50,4 @@ public class NewBankServer extends Thread{
 			}
 		}
 	}
-
 }
